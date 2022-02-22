1ac02567293b4bfd5e4985abd4df9128cefeac8e
Harish Pentapalli <harish5p@users.noreply.github.com>
2022-02-22 22:02:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def main():
     
     
     st.title("India Average Traffic Congestion")
-    plost.line_chart(df_tomtom_india_average, x='date', y=('congestion', 'congestion_ma'))
+    plost.line_chart(df_tomtom_india_average, x='date', y=('congestion'))
     
     
     
